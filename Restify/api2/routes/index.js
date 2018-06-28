05e18e14f6b5fd915f323a97cbe5888328d3fd71
Latsuj <justal.kevin@gmail.com>
2018-06-28 17:24:33 +0200

    Using custom query parameter for get

--- Restify/api2/routes/index.js
+++ Restify/api2/routes/index.js
@@ -107,27 +107,18 @@ module.exports = function(ctx) {
     	next();
     });
     
-    /**
-     * Read
-     */
-    server.get('/todos', (req, res, next) => {
-
-        let limit = parseInt(req.query.limit, 10) || 10, // default limit to 10 docs
-            skip  = parseInt(req.query.skip, 10) || 0, // default skip to 0 docs
-            query = req.query || {}
-
-        // remove skip and limit from query to avoid false querying
-        delete query.skip
-        delete query.limit
-
-        // find todos and convert to array (with optional query, skip and limit)
-        collection.find(query).skip(skip).limit(limit).toArray()
-            .then(docs => res.send(200, docs))
-            .catch(err => res.send(500, err))
-
-        next()
-
-    })
+    server.get('/article3', (req, res, next) => {
+    	// For letting the user use some custom query 
+    	let limit = parseInt(req.query.limit, 10) || 5,
+    		skip = parseInt(req.query.skip, 10) || 0,
+    		query = req.query || {};
+    		
+    	article.find({}).limit(limit).skip(skip).toArray( (err, docs) => {
+    		if(err) res.send(500, "Error on articles");
+    		res.send(200, docs);
+    	});	
+    	next();
+    });
 
     /**
      * Update
